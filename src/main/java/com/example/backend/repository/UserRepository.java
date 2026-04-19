8873a97ac2c100d21fa0807695b2ccddb3720833
WsilverW <906427984@qq.com>
2026-04-19 14:20:08 +0800

    redis修复

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -15,9 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 统计我有多少粉丝 (我是 following_id)
     @Query("SELECT COUNT(f) FROM Friendship f WHERE f.following.id = :userId")
-    long countFollowers(Long userId);
+    long countFollowers(@Param("userId") Long userId);
 
     // 统计我关注了多少人 (我是 follower_id)
     @Query("SELECT COUNT(f) FROM Friendship f WHERE f.follower.id = :userId")
-    long countFollowing(Long userId);
+    long countFollowing(@Param("userId") Long userId);
 }
